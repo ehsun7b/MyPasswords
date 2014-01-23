6aba735c0516fdb40d7916266573ca8e8f3b8809
Ehsun <post@ehsunbehravesh.com>
2014-01-23 17:45:29 +0800

    Adding home.html.

--- modules/GUI-Embeddedweb/src/main/java/com/mypasswords7/gui/embeddedweb/WebHandler.java
+++ modules/GUI-Embeddedweb/src/main/java/com/mypasswords7/gui/embeddedweb/WebHandler.java
@@ -4,11 +4,9 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 
 /**
  *
@@ -16,37 +14,61 @@ import java.util.Set;
  */
 public class WebHandler implements HttpHandler {
 
+  private static final int BUFFER_SIZE = 2048;
+  
   @Override
   public void handle(HttpExchange exchange) throws IOException {
     String requestMethod = exchange.getRequestMethod();
     URI requestURI = exchange.getRequestURI();
-    
+
     if (requestURI.getPath().equalsIgnoreCase("/")) {
-      responseHome();
+      responseHome(exchange);
     }
 
     /*
-    if (requestMethod.equalsIgnoreCase("GET")) {
-      Headers responseHeaders = exchange.getResponseHeaders();
-      responseHeaders.set("Content-Type", "text/plain");
-      exchange.sendResponseHeaders(200, 0);
-      try (OutputStream responseBody = exchange.getResponseBody()) {
-        Headers requestHeaders = exchange.getRequestHeaders();
-        Set<String> keySet = requestHeaders.keySet();
-        Iterator<String> iter = keySet.iterator();
-        while (iter.hasNext()) {
-          String key = iter.next();
-          List values = requestHeaders.get(key);
-          String s = key + " = " + values.toString() + "\n";
-          responseBody.write(s.getBytes());
-        }
-        responseBody.write("requestURI: ".concat(requestURI.getPath()).getBytes("UTF-8"));
-      }
-    }*/
+     if (requestMethod.equalsIgnoreCase("GET")) {
+     Headers responseHeaders = exchange.getResponseHeaders();
+     responseHeaders.set("Content-Type", "text/plain");
+     exchange.sendResponseHeaders(200, 0);
+     try (OutputStream responseBody = exchange.getResponseBody()) {
+     Headers requestHeaders = exchange.getRequestHeaders();
+     Set<String> keySet = requestHeaders.keySet();
+     Iterator<String> iter = keySet.iterator();
+     while (iter.hasNext()) {
+     String key = iter.next();
+     List values = requestHeaders.get(key);
+     String s = key + " = " + values.toString() + "\n";
+     responseBody.write(s.getBytes());
+     }
+     responseBody.write("requestURI: ".concat(requestURI.getPath()).getBytes("UTF-8"));
+     }
+     }*/
   }
 
-  private void responseHome() {
-    
+  private void responseHome(HttpExchange exchange) throws IOException {
+    if (exchange.getRequestMethod().equalsIgnoreCase("GET")) {
+      Headers responseHeaders = exchange.getResponseHeaders();
+      responseHeaders.set("Content-Type", "text/html");
+      exchange.sendResponseHeaders(200, 0);
+
+      try (OutputStream os = exchange.getResponseBody()) {
+        try (InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("home.html")) {
+          byte[] buffer = new byte[BUFFER_SIZE];
+          int len;
+          
+          while ((len = is.read(buffer)) > 0) {
+            os.write(buffer, 0, len);
+          }
+        }
+      }
+      
+      exchange.getResponseBody().close();
+
+    } else {
+      Headers responseHeaders = exchange.getResponseHeaders();
+      exchange.sendResponseHeaders(405, 0);
+      exchange.getResponseBody().close();
+    }
   }
 
 }
