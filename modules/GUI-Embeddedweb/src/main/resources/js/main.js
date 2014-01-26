c7aed579ddd44b1afe39a58421ad9c8767f5fa4d
ehsun7b <ehsun7b@gmail.com>
2014-01-26 16:20:44 +0800

    Adding some UI features (check database)

--- modules/GUI-Embeddedweb/src/main/resources/js/main.js
+++ modules/GUI-Embeddedweb/src/main/resources/js/main.js
@@ -1,7 +1,36 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(function() {
+  checkEngines();
+});
 
+function checkEngines() {
+  $("#dlgEnginCheck").dialog({
+    modal: true
+  });
 
+  $.ajax({
+    url: "/database",
+    type: "GET",
+    success: function(data, textStatus, jqXHR) {
+      if (data.successMessage) {
+        alert(data.successMessage);
+      }
+      
+      var select = $("form#frmLogin #engine");
+      
+      if (data.engines !== null && data.engines !== undefined) {
+        for (var i = 0; i < data.engines.length; ++i) {
+          var engine = data.engines[i];
+          select.append($("<option/>", {value: engine.engineName}).append(engine.engineName));
+        }
+      }
+
+      $("#dlgLogin").dialog({
+        modal: true,
+        width: "350px"
+      });
+      
+    }
+  }).done(function() {
+    $("#dlgEnginCheck").dialog('close');
+  });
+}
