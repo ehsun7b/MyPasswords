82e2a7cbf8ff91ed931afd398daed3f5d6957a5f
Ehsun <post@ehsunbehravesh.com>
2014-03-05 10:23:09 +0800

    delete response/DeleteEntryResponse.java/com/mypasswords7/gui/embeddedweb/response/DeleteEntryResponse.java

---
+++ modules/gui-embedded-web/GUI-Embeddedweb/src/main/java/com/mypasswords7/gui/embeddedweb/response/DeleteEntryResponse.java
@@ -0,0 +1,13 @@
+package com.mypasswords7.gui.embeddedweb.response;
+
+/**
+ *
+ * @author ehsun.behravesh
+ */
+public class DeleteEntryResponse extends Response {
+
+  public DeleteEntryResponse(boolean success) {
+    super(success);
+  }
+  
+}
